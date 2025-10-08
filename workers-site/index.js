397b1183764bb92a03af77a92b2124eaacfffa59
RodrigoAGuspian <rodalejo8@gmail.com>
2025-10-08 03:01:29 -0500

    fix

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,14 +1,15 @@
-addEventListener('fetch', event => {
+addEventListener("fetch", event => {
   event.respondWith(
     handleRequest(event.request)
   )
 })
 
 async function handleRequest(request) {
-  try {
-    return await fetch(request)
-  } catch {
-    const url = new URL(request.url)
+  const url = new URL(request.url)
+
+  if (!url.pathname.includes('.')) {
     return fetch(`${url.origin}/index.html`)
   }
+
+  return fetch(request)
 }
