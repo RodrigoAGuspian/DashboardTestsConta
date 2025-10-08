51dd41fa385fa7c09092e7345c8e3a49d46b33cb
RodrigoAGuspian <rodalejo8@gmail.com>
2025-10-08 02:49:04 -0500

    fix

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,13 +1,13 @@
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request))
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+
+addEventListener('fetch', event => {
+  event.respondWith(handleEvent(event))
 })
 
-async function handleRequest(request) {
-  const url = new URL(request.url)
-  
+async function handleEvent(event) {
   try {
-    return await fetch(`${url.origin}${url.pathname}`)
-  } catch {
-    return fetch(`${url.origin}/index.html`)
+    return await getAssetFromKV(event)
+  } catch (e) {
+    return await getAssetFromKV(event, { mapRequestToAsset: req => new Request('/index.html', req) })
   }
 }
